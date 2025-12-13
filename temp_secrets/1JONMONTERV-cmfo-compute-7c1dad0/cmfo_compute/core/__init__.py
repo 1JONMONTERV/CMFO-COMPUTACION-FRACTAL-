39bef93b2ed5ba0523f65b52d064e6f06b02faf8
Wrath343 <bryan_m007@hotmail.com>
2025-12-13 11:48:30 -0600

    fix(ci): Remove temp_secrets and duplicate test_cmfo.py

--- temp_secrets/1JONMONTERV-cmfo-compute-7c1dad0/cmfo_compute/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .t7_tensor import T7Tensor
-from .gamma_phi import gamma_step
-
-__all__ = ["T7Tensor", "gamma_step"]
--- temp_secrets/1JONMONTERV-cmfo-compute-7c1dad0/tests/test_api.py
+++
@@ -1,7 +0,0 @@
-import cmfo_compute as cmfo
-
-
-def test_logic():
-    assert cmfo.phi_and(1, -1) == -1
-    assert cmfo.phi_or(-1, 1) == 1
-    assert cmfo.phi_nand(1, 1) == -1

3aa20febdf9d141f8fca2a505993235d98ccefec
Jonnathan Montero Víquez <jmvlavacar@hotmail.com>
2025-12-14 01:03:29 -0600

    Fix: Make setup.py robust to relative paths for CI

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,4 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+# Robustly find the README in the root directory
+here = os.path.abspath(os.path.dirname(__file__))
+root_readme = os.path.join(here, "..", "..", "README.md")
+
+try:
+    with open(root_readme, encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "CMFO: Continuous Modal Fractal Oscillation Engine"
 
 setup(
     name="cmfo",
@@ -6,7 +17,7 @@ setup(
     author="Jonathan Montero Viques",
     author_email="jesuslocopor@gmail.com",
     description="Fractal Universal Computation Engine",
-    long_description=open("../../README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/1JONMONTERV/CMFO-COMPUTACION-FRACTAL-",
     packages=find_packages(),
